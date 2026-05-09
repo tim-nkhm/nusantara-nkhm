f806dc65dcbe772c0a99ad1129902f2447a1b2b4
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-05-09 22:53:55 +0700

    Add initial question bank to questions.py

---
+++ questions.py
@@ -0,0 +1,9 @@
+QUESTION_BANK = [
+    {
+        "text": "Siapa yang membacakan teks proklamasi?",
+        "options": ["Soekarno", "Moh Hatta", "Soekarno-Hatta", "Ahmad Soebarjo"],
+        "correct": "Soekarno-Hatta",
+        "type": "IQ",
+        "national": True
+    },
+]
